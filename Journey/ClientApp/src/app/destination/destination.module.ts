11e21e5ebee637f9a39932224a39c483fefddeae
Pasan Peiris <pasan.peiris01@gmail.com>
2020-03-31 18:45:05 +0530

    destination module creation

---
+++ Journey/ClientApp/src/app/destination/destination.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShortDescriptionComponent } from './short-description/short-description.component';
+
+
+
+@NgModule({
+  declarations: [ShortDescriptionComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DestinationModule { }
