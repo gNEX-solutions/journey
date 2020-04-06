96d113c0ef5ce5b9478b45740dc249695ffbbf24
Pasan Peiris <pasan.peiris01@gmail.com>
2020-04-06 14:44:43 +0530

    home screen ui initial commit

--- Journey/ClientApp/src/app/app-routing.module.ts
+++ Journey/ClientApp/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { DestinationModule } from './destination/destination.module';
 import { DestinationComponent } from './destination/destination/destination.component';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes =[
-  { path: '', redirectTo: '/destination', pathMatch: 'full'},
-  {path: 'destination', component: DestinationComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'destination', component: DestinationComponent},
+  {path: 'home', component: HomeComponent}
 ]
 
 @NgModule({
@@ -14,6 +17,7 @@ const routes: Routes =[
   imports: [
     CommonModule,
     DestinationModule,
+    HomeModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
---
+++ Journey/ClientApp/src/app/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { TopComponent } from './top/top.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatGridListModule } from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, TopComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatGridListModule
+  ]
+})
+export class HomeModule { }
