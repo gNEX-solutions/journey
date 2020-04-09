4e0208c1b0ad4304d5a4534179b66db1fedb6567
dinith72 <jdinith72@gmail.com>
2020-04-09 11:35:01 +0530

    changing the user model

--- Journey/Models/User.cs
+++ Journey/Models/User.cs
@@ -2,14 +2,42 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Journey.Models
 {
+    using System;
+    using System.Collections.Generic;
     public class User
     {
-        public int Id { get; set; }
-        [Column(TypeName = "varchar(MAX)")]
-        public string Username { get; set; }
-        [Column(TypeName = "varchar(MAX)")]
+        //    public int Id { get; set; }
+        //    [Column(TypeName = "varchar(MAX)")]
+        //    public string Username { get; set; }
+        //    [Column(TypeName = "varchar(MAX)")]
+        //    public string Password { get; set; }
+        //    [Column(TypeName = "varchar(MAX)")]
+        //    public string RefreshToken { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            this.FollwedBies = new HashSet<FollwedBy>();
+            this.UserFeedbacks = new HashSet<UserFeedback>();
+            this.WishLIsts = new HashSet<WishLIst>();
+        }
+
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public Nullable<System.TimeSpan> Birthdate { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public byte[] Username { get; set; }
         public string Password { get; set; }
-        [Column(TypeName = "varchar(MAX)")]
-        public string RefreshToken { get; set; }
+        public byte IsDeleted { get; set; }
+        public Nullable<System.DateTime> CreatedAt { get; set; }
+        public Nullable<System.DateTime> ModifiedAt { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<FollwedBy> FollwedBies { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UserFeedback> UserFeedbacks { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<WishLIst> WishLIsts { get; set; }
     }
 }
